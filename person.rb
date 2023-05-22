5b74623a86256512f4e71fb12f0e895cee9bf0e7
Santiago <santi.ponsle@gmail.com>
2023-05-22 20:00:29 -0300

    Corrected reviewers changes request

--- person.rb
+++ person.rb
@@ -9,12 +9,14 @@ class Person
     @parent_permission = parent_permission
   end
 
+  private
   def of_age
     @age >= 18
   end
 
+  public
   def can_use_services?
-    @parent_permission == true
+    of_age || @parent_permission
   end
 end
 
